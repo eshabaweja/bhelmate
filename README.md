8f18dba6c0e2acf388f324fcbcc30336f547f12d
eshabaweja <bawejaesha02@gmail.com>
2023-07-15 22:23:14 +0530

    :rocket: Flask-Rasa integration with basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BHELmate
-- created with Rasa and Flask
+- created with Rasa and [Flask]("https://medium.com/featurepreneur/integrating-chatbot-with-website-rasa-flask-4569f18d31be")
--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os, sys, requests, json
+from random import randint
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('chat.html')
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("chat.html")
+
+
+@app.route("/parse", methods=["POST", "GET"])
+def extract():
+    text = str(request.form.get("value1"))
+    payload = json.dumps({"sender": "user", "message": text})
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+    response = requests.request(
+        "POST",
+        url="http://localhost:5005/webhooks/rest/webhook",
+        headers=headers,
+        data=payload,
+    )
+    response = response.json()
+    # print("Response from Rasa:", response)
+    resp = []
+    for i in range(len(response)):
+        try:
+            resp.append(response[i]["text"])
+        except:
+            continue
+    result = resp
+    print("result type: ", type(result))
+    # return render_template("chat.html", result=result, text=text)
+    return result
+
+
+if __name__ == "__main__":
     app.run(debug=True)
